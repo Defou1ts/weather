928aada679daa0e71b19911ce18d5700901ca149
Defou1ts <pasnkevikn@gmail.com>
2023-04-05 13:56:54 +0300

    feat: initialized redux

---
+++ src/store/store.ts
@@ -0,0 +1,33 @@
+import { FLUSH, PAUSE, PERSIST, persistReducer, persistStore, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { getPersistConfig } from 'redux-deep-persist';
+
+import type { ThunkAction, Action } from '@reduxjs/toolkit';
+
+const rootReducer = combineReducers({});
+
+const persistConfig = getPersistConfig({
+	key: 'root',
+	storage,
+	blacklist: [],
+	rootReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({
+			serializableCheck: {
+				ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+			},
+		}),
+});
+
+export const persistor = persistStore(store);
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
