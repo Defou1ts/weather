dbcee5f36f0eca8bd643eef8d4d5164e4c6b1339
Defou1ts <pasnkevikn@gmail.com>
2023-04-05 13:50:02 +0300

    feat: configured base webpack config for React

---
+++ src/index.tsx
@@ -0,0 +1,18 @@
+import { createRoot } from 'react-dom/client';
+
+import { App } from './App';
+
+const container = document.getElementById('root');
+
+if (container === null) {
+	throw new Error('Container element not found');
+}
+
+const root = createRoot(container);
+
+root.render(
+	<>
+		<App />
+		<h1>Hello111</h1>
+	</>
+);
---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const path = require('path');
+
+const paths = {
+	src: path.resolve(__dirname, 'src'),
+	dist: path.resolve(__dirname, 'dist'),
+	public: path.resolve(__dirname, 'public'),
+};
+
+module.exports = {
+	context: paths.src,
+
+	entry: {
+		app: './index',
+	},
+
+	output: {
+		path: paths.dist,
+		filename: '[name].bundle.js',
+	},
+
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.jsx'],
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+			'@assets': path.resolve(__dirname, 'src/assets'),
+			'@types': path.resolve(__dirname, 'src/types'),
+			'@components': path.resolve(__dirname, 'src/components'),
+			'@constants': path.resolve(__dirname, 'src/constants'),
+			'@pages': path.resolve(__dirname, 'src/pages'),
+			'@store': path.resolve(__dirname, 'src/store'),
+			'@utils': path.resolve(__dirname, 'src/utils'),
+			'@hooks': path.resolve(__dirname, 'src/hooks'),
+			'@styles': path.resolve(__dirname, 'src/assets/styles'),
+			'@images': path.resolve(__dirname, 'src/assets/images'),
+			'@interfaces': path.resolve(__dirname, 'src/interfaces'),
+			'@theme': path.resolve(__dirname, 'src/theme'),
+		},
+	},
+
+	devtool: 'inline-source-map',
+
+	module: {
+		rules: [
+			{
+				test: /\.svg$/,
+				use: ['@svgr/webpack'],
+			},
+			{
+				test: /\.tsx?$/,
+				loader: 'ts-loader',
+			},
+			{
+				test: /\.?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env', '@babel/preset-react'],
+					},
+				},
+			},
+		],
+	},
+
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin(
+			Object.assign(
+				{},
+				{
+					template: path.resolve(paths.public, 'index.html'),
+				},
+				process.env.NODE_ENV === 'production'
+					? {
+							minify: {
+								removeComments: true,
+								collapseWhitespace: true,
+								removeRedundantAttributes: true,
+								useShortDoctype: true,
+								removeEmptyAttributes: true,
+								removeStyleLinkTypeAttributes: true,
+								keepClosingSlash: true,
+								minifyJS: true,
+								minifyCSS: true,
+								minifyURLs: true,
+							},
+					  }
+					: undefined
+			)
+		),
+	],
+};
