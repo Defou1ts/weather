31d30936bf344a611f763bf03f356da94e5b068b
Defou1ts <pasnkevikn@gmail.com>
2023-04-06 12:34:01 +0300

    fix: api config

--- src/api/openweather.api.ts
+++ src/api/openweather.api.ts
@@ -8,15 +8,28 @@ const token = process.env.REACT_APP_OPENWEATHER_API_KEY as string;
 
 const openWeatherApiInstance: AxiosInstance = axios.create({
 	baseURL: `https://api.openweathermap.org/data/2.5`,
+	params: {
+		appid: token,
+		units: 'metric',
+	},
 });
 
 export const openWeatherApi = {
-	async getTodayWeather(city: string) {
+	async getTodayWeatherByCityName(city: string) {
 		const { data } = await openWeatherApiInstance.get<TodayWeather>('/weather', {
 			params: {
 				q: city,
-				appid: token,
-				units: 'metric',
+			},
+		});
+
+		return data;
+	},
+
+	async getTodayWeatherByCoords(lon: number, lat: number) {
+		const { data } = await openWeatherApiInstance.get<TodayWeather>('/weather', {
+			params: {
+				lat,
+				lon,
 			},
 		});
 
