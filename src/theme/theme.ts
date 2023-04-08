f97f3d1ef8a6eeb53a6f22a4e74d273cfcd6e279
Defou1ts <pasnkevikn@gmail.com>
2023-04-08 16:58:47 +0300

    fix: theme object

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -7,10 +7,10 @@ export const colors = {
 } as const;
 
 export const borderRadius = [5, 10, 20] as const;
-export const width = [139, 1110, 100, 60] as const;
-export const height = [38, 674, 100, 38, 300, 60] as const;
+export const width = [90, 1110, 100, 60] as const;
+export const height = [30, 674, 100, 38, 300, 60] as const;
 export const padding = [50, 70, 15, 10] as const;
-export const fontSize = [17, 20, 23, 25, 30, 41] as const;
+export const fontSize = [17, 20, 19, 25, 30, 41] as const;
 export const gaps = [28, 36, 39, 48, 58] as const;
 export const margin = [20, 250, 150] as const;
 export const blur = [7.5] as const;
