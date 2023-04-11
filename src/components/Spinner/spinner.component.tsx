2e74729f54bfe4fe43672e9cf84e37e25d928cf3
Defou1ts <pasnkevikn@gmail.com>
2023-04-11 09:55:17 +0300

    feat added spinner wrapper

--- src/components/Spinner/spinner.component.tsx
+++ src/components/Spinner/spinner.component.tsx
@@ -1,10 +1,14 @@
 import { getSpinnerSize } from '@utils';
 import { spinnerIcon } from '@assets';
 
-import { SpinnerImage } from './styled';
+import { SpinnerImage, SpinnerWrapper } from './styled';
 
 import type { SpinnerProps } from './interfaces';
 
 export const Spinner = ({ size }: SpinnerProps) => {
-	return <SpinnerImage src={spinnerIcon} size={getSpinnerSize(size)} alt="Loading" title="Loading" />;
+	return (
+		<SpinnerWrapper>
+			<SpinnerImage src={spinnerIcon} size={getSpinnerSize(size)} alt="Loading" title="Loading" />
+		</SpinnerWrapper>
+	);
 };
