2f72ec25b09d4fb93adc7d7ff50eebb4933fe922
Defou1ts <pasnkevikn@gmail.com>
2023-04-13 21:36:04 +0300

    feat: tests, updated packages

--- cypress/support/weather.ts
+++ cypress/support/weather.ts
@@ -1,6 +1,13 @@
 export const checkClockView = () => {
 	it('Check clock view', () => {
 		cy.visit('/');
-		cy.get('[data-test-id=time-clock]').should('exist');
+		cy.wait(5);
+		cy.get('[data-test-id=time-clock]')
+			.should('be.visible')
+			.then((value) => {
+				const text = value.text();
+				const minutes = +text.slice(0, -2).split(':')[1];
+				expect(minutes).to.equal(new Date().getMinutes());
+			});
 	});
 };
