ca98632261f9542e91bb6e8259e1338a4db6d8a5
Defou1ts <pasnkevikn@gmail.com>
2023-04-17 17:28:28 +0300

    fix: today weather data transform

--- src/utils/transform.ts
+++ src/utils/transform.ts
@@ -5,7 +5,24 @@ import type {
 	DailyWeatherResponse,
 	HourlyWeather,
 	HourlyWeatherResponse,
+	TodayWeather,
+	TodayWeatherResponse,
 } from '@interfaces';
+
+export const transformTodayWeatherResponse = (todayWeatherResponse: TodayWeatherResponse): TodayWeather => {
+	const { weather, main, name: cityName } = todayWeatherResponse;
+	const { icon: iconCode, description, id } = weather[0];
+	const { feels_like: feelsLike, temp: temperature } = main;
+
+	return {
+		iconCode,
+		description,
+		feelsLike,
+		temperature,
+		cityName,
+		id,
+	};
+};
 
 export const transformDailyWeatherResponse = (dailyWeatherResponse: DailyWeatherResponse): DailyWeather => {
 	const {
